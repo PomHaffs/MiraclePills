bb7962d5750fce2cdc82f7a0babd6ffb1f7c06fe
PomHaffs <tomashaffenden@icloud.com>
2016-10-31 12:30:26 +1100

    first commit

--- Miracle Pill/ViewController.swift
+++ Miracle Pill/ViewController.swift
@@ -8,18 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
+    @IBOutlet weak var statePicker: UIPickerView!
+    
+    @IBOutlet weak var statePickerBtn: UIButton!
+    
+    let states = ["New South Wales", "Queensland", "South Australia", "Tasmania", "Victoria", "Western Australia"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        statePicker.dataSource = self
+        statePicker.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    @IBAction func stateButtonPressed(_ sender: Any) {
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
 
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) ->
+        Int {
+            return states.count
+        }
+        
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent
+        component: Int) -> String? {
+        return states[row]
+    }
 
 }
 
